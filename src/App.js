3680bd29d977c859c8dedcca112c6629a46c9b12
drownedout <danielcdonnelly@gmail.com>
2018-05-13 14:17:44 -0400

    Hooked up API to actions and reducers for retrieving questions and users,added redux-handle logger middleware

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { connect } from 'react-redux'
+import { handleInitialData } from './actions/shared';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
   render() {
+    const { loading } = this.props;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Would You Rather?</h1>
+        {loading === true ?
+          null :
+          <h2></h2>
+        }
       </div>
     );
   }
 }
 
-export default App;
+export default connect()(App)
