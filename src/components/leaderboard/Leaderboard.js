a872ea9cb554fd55f53c2f8a33eac3883109bc7a
drownedout <danielcdonnelly@gmail.com>
2018-05-26 18:45:59 -0400

    Wired up leaderboard and added methods to user reducers

--- src/components/leaderboard/Leaderboard.js
+++ src/components/leaderboard/Leaderboard.js
@@ -1,13 +1,76 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  Grid,
+  Avatar,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Paper,
+  withStyles
+} from "@material-ui/core"
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  paper: {
+  	width: '90%',
+  	margin: '1rem auto',
+  	padding: '1rem'
+  },
+  table: {
+    minWidth: 700,
+  },
+});
+
 
 class Leaderboard extends Component {
   render(){
+  	const { users, totalScore, classes } = this.props
     return (
-      <div>
-        <h1>Leaderboard</h1>
-      </div>
+        <Grid className={classes.root} container>
+        	<Paper className={classes.paper}>
+        		<Table className={classes.table}>
+        			<TableHead>
+        				<TableRow>
+        					<TableCell>Rank</TableCell>
+        					<TableCell>Name</TableCell>
+        					<TableCell>Asked</TableCell>
+        					<TableCell>Answered</TableCell>
+        					<TableCell>Total Score</TableCell>
+        				</TableRow>
+        			</TableHead>
+        			<TableBody>
+        				{users.map((user, rank) => (
+        					<TableRow key={user.id}>
+		        				<TableCell>{rank+1}</TableCell>
+		        				<TableCell>{user.name}</TableCell>
+		        				<TableCell>{user.questions.length}</TableCell>
+		        				<TableCell>{Object.keys(user.answers).length}</TableCell>
+		        				<TableCell>{totalScore(user)}</TableCell>
+	        				</TableRow>
+        				))}
+        			</TableBody>
+        		</Table>
+        	</Paper>
+        </Grid>
     )
   }
 }
 
-export default Leaderboard;
+function mapStateToProps( { users }){
+	const totalScore = user => 
+	Object.keys(user.answers).length + user.questions.length;
+
+	return {
+		users: Object.values(users).sort((a,b) => totalScore(b) - totalScore(a)),
+		totalScore
+	}
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(Leaderboard));
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,12 +1,35 @@
 import { RECEIVE_USERS } from '../actions/users'
+import { HANDLE_ADD_ANSWER, HANDLE_ADD_QUESTION } from '../actions/questions';
 
 export default function users (state = {}, action) {
 	switch(action.type) {
-		case RECEIVE_USERS :
+		case RECEIVE_USERS:
 		  return {
 		    ...state,
 		    ...action.users
 		  }
+		case HANDLE_ADD_ANSWER:
+			const { answer, qid, authedUser } = action.info
+			const user = state[authedUser]
+			return {
+				...state,
+				[authedUser]: {
+					...user,
+					answers: {
+						...user.answers,
+						[qid]: answer
+					}
+				}
+			}
+		case HANDLE_ADD_QUESTION:
+			const author = state[action.info.author]
+			return {
+				...state,
+				[author.id]: {
+					...author,
+					questions: author.questions.concat([action.info.id])
+				}
+			}
 		default :
 		  return state
 	}
